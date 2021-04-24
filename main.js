0701b22d5de90c98eb800a0e24e90dfb711a12db
Khuldoonqataesheh <kqataesheh@gmail.com>
2021-04-24 18:27:20 +0300

    initial shape

--- main.js
+++ main.js
@@ -1,3 +1,25 @@
-array.forEach(element => {
-    
+document.addEventListener("DOMContentLoaded", () => {
+  const cells = document.querySelectorAll(".grid-container div");
+  const result = document.querySelector("#result");
+  const displayNumberOfPlayer = document.querySelector("#numberofplayer");
+  let numberOfPlayer = 1;
+  for (let i = 0; i < cells.length; i++) {
+    cells[i].onclick = () => {
+      if (cells[i + 7].classList.contains("filled")) {
+        if (numberOfPlayer === 1) {
+          cells[i].classList.add("filled");
+          cells[i].classList.add("player_1");
+          numberOfPlayer = 2;
+          displayCurrentPlayer.innerHTML = numberOfPlayer;
+        } else if (numberOfPlayer === 2) {
+          cells[i].classList.add("filled");
+          cells[i].classList.add("player_2");
+          numberOfPlayer = 1;
+          displayNumberOfPlayer.innerHTML = numberOfPlayer;
+        }
+      } else {
+        alert("Unavailable");
+      }
+    };
+  }
 });
